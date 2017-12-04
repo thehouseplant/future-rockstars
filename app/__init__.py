4a3a3c1851e5e679b9351a89ab2718c203c77263
Sean Collins <sean.collins@outlook.com>
2017-12-03 18:06:33 -0800

    Updated init for better application bootstrapping

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,12 @@
 import json
 import os
 from flask import Flask
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 
-CUR_DIR = os.path.realpath(os.path.dirname(__file__))
-app = Flask(
-    __name__,
-    static_folder=os.path.join(CUR_DIR, 'static'),
-    template_folder=os.path.join(CUR_DIR, 'templates'))
-app.config.update(
-    DEBUG=True
-)
 
 # Initialize the database with boilerplate JSON
-db = TinyDB('../db.json')
+db_location = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'db.json'))
+db = TinyDB(db_location)
 db.purge()
 with open('./data/bands.json') as bands_data:
     bands = json.load(bands_data)
@@ -25,6 +18,18 @@ with open('./data/dorms.json') as dorms_data:
     dorms = json.load(dorms_data)
     db.insert_multiple(dorms)
 
-app = Flask(__name__)
+
+# Define paths and configure application
+CUR_DIR = os.path.realpath(os.path.dirname(__file__))
+app = Flask(
+    __name__,
+    static_folder=os.path.join(CUR_DIR, 'static'),
+    template_folder=os.path.join(CUR_DIR, 'templates'))
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='SomethingSuperSecret'
+)
+app.config['SECRET_KEY'] = 'SomethingSuperSecret'
+
 
 from app import views
