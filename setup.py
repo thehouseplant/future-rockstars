dce7fbe0219442db115e83636fa93f8d5dd45999
Sean Collins <sean.collins@outlook.com>
2017-12-03 18:00:33 -0800

    Added for easier project dependency installation

--- setup.py
+++ setup.py
@@ -8,12 +8,24 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'certifi==2017.11.5',
+        'chardet==3.0.4',
         'click==6.7',
+        'coverage==4.4.2',
+        'coveralls==1.2.0',
+        'docopt==0.6.2',
         'Flask==0.12.2',
+        'Flask-Script==2.0.6',
+        'Flask-Testing==0.6.2',
+        'Flask-WTF==0.14.2',
+        'idna==2.6',
         'itsdangerous==0.24',
         'Jinja2==2.9.6',
         'MarkupSafe==1.0',
-        'tinydb==3.5.0',
-        'Werkzeug==0.12.2'
+        'requests==2.18.4',
+        'tinydb==3.5.0'.
+        'urllib3==1.22',
+        'Werkzeug==0.12.2',
+        'WTForms==2.1'
     ]
 )
